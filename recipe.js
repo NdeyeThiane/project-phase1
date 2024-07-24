cbe4c3c63d3bc9b9eb5af2706e8e0ad13ee923ad
Ndeye Thiane <ndeyet2004@gmail.com>
2024-07-24 08:45:01 -0700

    set up bootstrap and add a new ingredient

---
+++ recipe.js
@@ -0,0 +1,32 @@
+let newListItem = document.createElement("li");
+newListItem.innerHTML = "Sugar";
+document.getElementById("ingredients").appendChild(newListItem);
+
+//let ul = document.getElementsByTagName("ul");
+let div = document.getElementsByClassName("divLi");
+/*
+
+let label = document.createElement("label");
+let check = document.createElement("input");
+
+
+
+for(let i = 0; i<ul.length; i++){
+  check.type="checkbox";
+  check.id="check";
+  check.name="checkb";
+  label.innerHTML = ul[i];
+  label.htmlFor ="check";
+  ul.appendChild(label);
+  ul.appendChild(check);
+}
+div.appendChild(ul);
+*/
+
+// let aTag = document.getElementsByTagName("tagsForRecipe");
+
+// for(let i = 0; i<aTag.length; i++){
+//     aTag[i].addEventListener('mouseenter', function () {
+//     document.body.style.color ="green";
+//   })
+// }
